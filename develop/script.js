a641ad1ecd346b7591cdee4f0b56d9a7ccc3440c
uchrissd <uchrissd@protonmail.com>
2019-12-18 16:41:48 -0800

    completed function for adding new roles to database

--- develop/script.js
+++ develop/script.js
@@ -48,6 +48,7 @@ function runProgram() {
           break;
 
         case "Add an employee?":
+          // Needs to call getMangerNames first to run a query to get the info, then runs another function to add employee to database
           addEmployee();
           break;
 
@@ -83,15 +84,6 @@ function runProgram() {
 }
 
 function addEmployee() {
-  let managerNames = [];
-  let managersInfo;
-  connection.query("SELECT * FROM manager", function(err, res) {
-    if (err) throw err;
-    managersInfo = res;
-    res.forEach(element => {
-      managerNames.push(element.first_name + " " + element.last_name);
-    });
-  });
   inquirer
     .prompt([
       {
@@ -108,18 +100,18 @@ function addEmployee() {
         name: "role",
         type: "list",
         message: "What is their role?",
-        choices: ["Engineer", "Accountant", "Lawyer", "Sales Rep"]
+        choices: [{ name: "Role test", value: 1 }]
       },
       {
         name: "manager",
         type: "list",
         message: "Who is their manager?",
-        choices: managerNames
+        choices: ["Manager name"]
       }
     ])
     .then(function(answer) {
       // when finished prompting, insert a new item into the db with that info
-      let managerId = saveManagerIdToEmployee(answer.manager, managersInfo);
+      // let managerId = saveManagerIdToEmployee(answer.manager, managersInfo);
       console.log("this is the manager answer", saveManagerIdToEmployee);
       connection.query(
         "INSERT INTO employee SET ?",
@@ -178,33 +170,110 @@ function viewDepartments() {
 }
 
 function viewRoles() {
-  connection.query("SELECT * FROM roles", function(err, res) {
-    if (err) throw err;
-
-    // Log all results of the SELECT statement
-    console.log("this is the res", res);
-    let roleId = [];
-    res.forEach(e => {
-      roleId.push(e.id);
+  getRoleResponse()
+    .then(function(res) {
+      console.table(res);
+    })
+    .catch(function(err) {
+      console.log("Promise rejection error: " + err);
     });
-    console.log("this is the role id", roleId);
-    console.table(res);
-    //   res.forEach(e => {
-    //     console.log(e.name);
-    //   });
-  });
 }
+
+function addRole() {
+  getDepartments()
+    .then(function(res) {
+      console.log("this is the departments res", res);
+      inquirer
+        .prompt([
+          {
+            name: "title",
+            type: "input",
+            message: "What is the title of the new role"
+          },
+
+          {
+            name: "salary",
+            type: "input",
+            message: "What is the new role's salary?"
+          },
+
+          {
+            name: "department",
+            type: "list",
+            message: "Which department will this new role be a part of?",
+            choices: res
+          }
+        ])
+        .then(function(answer) {
+          console.log(answer.department);
+          let deptId;
+          res.forEach(e => {
+            if (e.name === answer.department) {
+              deptId = e.id;
+            }
+          });
+          connection.query(
+            "INSERT INTO roles SET ?",
+            {
+              title: answer.title,
+              salary: answer.salary,
+              department_id: deptId
+            },
+            function(err) {
+              if (err) throw err;
+            }
+          );
+        });
+    })
+    .catch(function(err) {
+      console.log("Promise rejection error: " + err);
+    });
+}
+
+// function getRoleResponse() {
+//   connection.query("SELECT * FROM roles", function(err, res) {
+//     if (err) throw err;
+//     return res;
+//   });
+// }
+
+getRoleResponse = function() {
+  return new Promise(function(resolve, reject) {
+    connection.query("SELECT * FROM roles", function(err, res) {
+      if (err) throw err;
+
+      // return res;
+      resolve(res);
+    });
+  });
+};
+
+getDepartments = function() {
+  return new Promise(function(resolve, reject) {
+    connection.query("SELECT * FROM department", function(err, res) {
+      if (err) throw err;
+
+      // return res;
+      resolve(res);
+    });
+  });
+};
+// getEmployeeNames = function() {
+//   return new Promise(function(resolve, reject) {
+//     connection.query("SELECT Name, Surname FROM Employee", function(err, rows) {
+//       if (rows === undefined) {
+//         reject(new Error("Error rows is undefined"));
+//       } else {
+//         resolve(rows);
+//       }
+//     });
+//   });
+// };
 
 function viewEmployees() {
   connection.query("SELECT * FROM employee", function(err, res) {
     if (err) throw err;
-
-    // Log all results of the SELECT statement
-
     console.table(res);
-    //   res.forEach(e => {
-    //     console.log(e.name);
-    //   });
   });
 }
 
@@ -217,10 +286,9 @@ function viewManagers() {
   });
 }
 
-function saveManagerIdToEmployee(name, objects) {
-  console.log(name, objects);
-
-  objects.forEach(e => {
+function saveManagerIdToEmployee(name, managerArray) {
+  managerArray.forEach(e => {
+    // console.log(e);
     if (e.first_name + " " + e.last_name === name) {
       return e.id;
     }
